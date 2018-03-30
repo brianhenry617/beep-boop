03159cecca0bb3cb3acffc51a30484b5292cf962
Brian Henry <94brianhenry@gmail.com>
2018-03-30 09:39:27 -0700

    Add variables and booleans for beep boop and sorry, doesnt do anything yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,22 @@
-// USER INTERFACE
 $(document).ready(function() {
   $("form#inputEnglish").submit(function(event) {
     event.preventDefault();
+    var english = $("input#inputEnglish").val();
+    if (english===0) {
+    return "Beep! . . ."(inputEnglish);
+  }else if (inputEnglish===1){
+    return "Boop! . . ."(inputEnglish);
+  }else if (inputEnglish = /3){
+    return "I'm sorry, Dave. I'm afraid I can't do that."(inputEnglish);
+});
+});
+});
+
+
+
+
+
+// USER INTERFACE
     var english = $("input#inputEnglish").val();
     english = english.toUpperCase();
 });
