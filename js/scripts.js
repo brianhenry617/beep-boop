839c62e7ded2ba192d340ae6038f6298f713519e
Brian Henry <94brianhenry@gmail.com>
2018-03-30 15:45:42 -0700

    readjust the order of elses and ifs

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,15 @@ $(document).ready(function() {
     var english = $("input#numbers").val();
     var result = "";
     var num = parseInt(english);
-    if (english.search("0") != -1) {
-      result = "Beep! . . .";
+    if (num %3==0) {
+      result = "I'm sorry, Dave. I'm afraid I can't do that.";
     }else if (english.search("1") != -1) {
       result = "Boop! . . .";
-    } else if (num %3==0) {
-      result = "I'm sorry, Dave. I'm afraid I can't do that.";
+    } else if (english.search("0") != -1) {
+    result = "Beep! . . .";
     }else {
       result = num;
-
     }
-    // }else if (inputEnglish===%3){
-    // return "I'm sorry, Dave. I'm afraid I can't do that."(inputEnglish);
-    // var output = return;
     alert(result);
   });
 })
