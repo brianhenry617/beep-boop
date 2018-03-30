14f4e8b532dd4b75dd1feee70326dd2e48e30d62
Brian Henry <94brianhenry@gmail.com>
2018-03-30 15:01:49 -0700

    Add some adjustments to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,17 @@
-$(document).ready(function() {
-  $("form#inputEnglish").submit(function(event) {
-    event.preventDefault();
-    var english = $("input#inputEnglish").val();
-    if (english===0) {
-    return "Beep! . . ."(inputEnglish);
-  }else if (inputEnglish===1){
-    return "Boop! . . ."(inputEnglish);
-  }else if (inputEnglish = /3){
-    return "I'm sorry, Dave. I'm afraid I can't do that."(inputEnglish);
-});
-});
-});
-
-
-
-
-
 // USER INTERFACE
-    var english = $("input#inputEnglish").val();
-    english = english.toUpperCase();
-});
-});
-    $("#result").show();
-        $("#output").text(output);
-        //hides result with click
-        $("#inputEnglish").click(function(){
-            $("#beepBoop").fadeOut("slow");
-
-          });
+$(document).ready(function() {
+  $("form#numbers").submit(function(event) {
+    event.preventDefault();
+    var english = parseInt($("input#numbers").val());
+    var result = "";
+    if (english===0) {
+      result = "Beep! . . .";
+    }else if (english===1){
+      result = "Boop! . . .";
+    }
+    // }else if (inputEnglish===%3){
+    // return "I'm sorry, Dave. I'm afraid I can't do that."(inputEnglish);
+    // var output = return;
+    alert(result);
+  });
+})
