440b3dcdc5358fb3fac2520f80b8ebb0bc75ac3d
Brian Henry <94brianhenry@gmail.com>
2018-03-30 15:19:02 -0700

    fixed the ifs and elses so that they work properly

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,18 @@
 $(document).ready(function() {
   $("form#numbers").submit(function(event) {
     event.preventDefault();
-    var english = parseInt($("input#numbers").val());
+    var english = $("input#numbers").val();
     var result = "";
-    if (english===0) {
+    var num = parseInt(english);
+    if (english.search("0") != -1) {
       result = "Beep! . . .";
-    }else if (english===1){
+    }else if (english.search("1") != -1) {
       result = "Boop! . . .";
+    } else if (num %3==0) {
+      result = "I'm sorry, Dave. I'm afraid I can't do that.";
+    }else {
+      result = num;
+
     }
     // }else if (inputEnglish===%3){
     // return "I'm sorry, Dave. I'm afraid I can't do that."(inputEnglish);
